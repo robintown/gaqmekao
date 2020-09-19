5748064d1e76440879ecd645d45aae69fd8b694f
dỏaqrēa <robintown@tcakoi.dev>
2020-09-19 20:44:16 +0000

    add all fields to StarDict exports

--- tools/tabfile.js
+++ tools/tabfile.js
@@ -8,15 +8,34 @@ process.chdir(__dirname);
 const dictionary = require('./../dictionary.json');
 
 const tabfile = dictionary.map(entry => {
-    const lines = [];
+    const sections = [];
 
-    lines.push(entry.toaq);
-    if (entry.type) lines.push(entry.type);
-    lines.push(entry.english);
-    if (entry.frame) lines.push(`frame: ${entry.frame}`);
-    if (entry.distribution) lines.push(`distribution: ${entry.distribution}`);
+    if (entry.type)
+      sections.push(entry.type);
+    sections.push(entry.english);
+    if (entry.gloss)
+      sections.push(`gloss: ${entry.gloss}`);
+    if (entry.keywords.length > 0)
+      sections.push(`keywords: ${entry.keywords.join(', ')}`);
+    if (entry.frame)
+      sections.push(`frame: ${entry.frame}${entry.namesake ? ' (namesake)' : ''}`);
+    if (entry.distribution)
+      sections.push(`distribution: ${entry.distribution}`);
+    if (entry.etymology)
+      sections.push(`etymology: ${entry.etymology}`);
+    if (entry.fields && entry.fields.length > 0)
+      sections.push(`fields: ${entry.fields.map(field => field.join(' ∨ ')).join(', ')}`);
+    if (entry.notes.length > 0)
+      sections.push(`notes:\\n\t${entry.notes.join('\\n\t')}`);
 
-    return `${entry.toaq}\t${lines.join('\\n')}`;
+    if (entry.examples.length > 0) {
+      const exLines = entry.examples.map(
+        example => `${example.toaq} — ${example.english}`
+      );
+      sections.push(`examples:\\n\t${exLines.join('\\n\t')}`);
+    }
+
+    return `${entry.toaq}\t${sections.join('\\n\\n')}`;
   }).join('\n');
 
 process.stdout.write(tabfile + '\n');
