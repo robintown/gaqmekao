4ec96f5bee4709289d29d8d0b4b7a3b9da7792e1
dỏaqrēa <robintown@tcakoi.dev>
2020-09-21 01:42:57 +0000

    escape backslashes in StarDict export data

--- tools/tabfile.js
+++ tools/tabfile.js
@@ -7,30 +7,32 @@
 process.chdir(__dirname);
 const dictionary = require('./../dictionary.json');
 
+const escape = text => text.replace(/\\/g, '\\\\');
+
 const definitions = dictionary.map(entry => {
     const sections = [];
 
     if (entry.type)
-      sections.push(entry.type);
-    sections.push(entry.english);
+      sections.push(escape(entry.type));
+    sections.push(escape(entry.english));
     if (entry.official)
       sections.push(`official`);
     if (entry.gloss)
-      sections.push(`gloss: ${entry.gloss}`);
+      sections.push(`gloss: ${escape(entry.gloss)}`);
     if (entry.frame)
-      sections.push(`frame: ${entry.frame}${entry.namesake ? ' (namesake)' : ''}`);
+      sections.push(`frame: ${escape(entry.frame)}${entry.namesake ? ' (namesake)' : ''}`);
     if (entry.distribution)
-      sections.push(`distribution: ${entry.distribution}`);
+      sections.push(`distribution: ${escape(entry.distribution)}`);
     if (entry.etymology)
-      sections.push(`etymology: ${entry.etymology}`);
+      sections.push(`etymology: ${escape(entry.etymology)}`);
     if (entry.fields && entry.fields.length > 0)
-      sections.push(`fields: ${entry.fields.map(field => field.join(' ∨ ')).join(', ')}`);
+      sections.push(`fields: ${entry.fields.map(field => field.map(escape).join(' ∨ ')).join(', ')}`);
     if (entry.notes.length > 0)
-      sections.push(`notes:\\n\t${entry.notes.join('\\n\t')}`);
+      sections.push(`notes:\\n\t${entry.notes.map(escape).join('\\n\t')}`);
 
     if (entry.examples.length > 0) {
       const exLines = entry.examples.map(
-        example => `${example.toaq} — ${example.english}`
+        example => `${escape(example.toaq)} — ${escape(example.english)}`
       );
       sections.push(`examples:\\n\t${exLines.join('\\n\t')}`);
     }
@@ -41,7 +43,7 @@ const definitions = dictionary.map(entry => {
 const keywords = [...new Set(dictionary.map(entry => entry.keywords).flat())]
   .map(keyword => {
     const keyed = dictionary.filter(entry => entry.keywords.includes(keyword));
-    return `${keyword}\t${keyed.map(entry => entry.toaq).join(', ')}`;
+    return `${keyword}\t${keyed.map(entry => escape(entry.toaq)).join(', ')}`;
   }).join('\n');
 
 const tabfile = [definitions, keywords].join('\n') + '\n';
